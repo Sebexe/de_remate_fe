7361dd920b9e4eae196c52220019995211bef897
lucasRubiniF1 <lrubini@uade.edu.ar>
2025-04-01 22:43:15 -0300

    feat: agrega EncryptedSharedPreferences al proyecto

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,22 +28,24 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
     kotlinOptions {
         jvmTarget = "11"
     }
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -57,7 +59,7 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.9.0")
 
-    // Security Crypto
+    // 🔐 EncryptedSharedPreferences
     implementation("androidx.security:security-crypto:1.1.0-alpha06")
 }
 
